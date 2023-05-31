a3d9405b926c0f1030b5613d0e8f79b14e496cfd
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2023-05-31 18:12:03 -0400

    Changed Purchase schema invoiceDate property type to String. New invoice creation now redirects to main purchase page

--- routes/purchase.routes.js
+++ routes/purchase.routes.js
@@ -16,15 +16,13 @@ router.get('/purchases/create', (req, res, next) => {
         .then((booksFromDb) => {
             Movie.find()
             .then((moviesFromDb) => {
-        res.render('purchases/new-invoice', {movies: moviesFromDb, customers: customerFromDB, books: booksFromDb});
+            res.render('purchases/new-invoice', {movies: moviesFromDb, customers: customerFromDB, books: booksFromDb});
+            })
         })
-        
-        })
-        
     })
-            .catch(err => {
-                console.log(err);
-            });    
+    .catch(err => {
+        console.log(err);
+    });    
 });
 
 router.post('/purchases/create', (req, res ,next) => {
@@ -39,26 +37,6 @@ router.post('/purchases/create', (req, res ,next) => {
         purchaseTotal: req.body.purchaseTotal,
     }
 
-    // Purchase.create(invoiceToCreate)
-    // .then(newlyCreatedInvoice => {
-    //     console.log(newlyCreatedInvoice)
-    //     res.redirect('/purchases/create', newlyCreatedInvoice);
-    // }).catch(err => {
-    //     res.redirect('/purchases/create');
-    // })
-
-
-    // Purchase.create(invoiceToCreate)
-    // .then(newlyCreatedInvoice => {
-    //     console.log('NEW INVOICE ID ===>', newlyCreatedInvoice._id)
-    //     return Customer.findByIdAndUpdate(customers, {$push: {purchases: newlyCreatedInvoice._id} });
-    // })
-    // .then(() => res.redirect('/purchases/create'))
-    // .catch(err => {
-    //     res.redirect('/purchases/create');
-    // })
-// ==============================================
-console.log('invoiceToCreate===>', invoiceToCreate)
     Purchase.create(invoiceToCreate)
     .then(newlyCreatedInvoice => {
         console.log('NEW INVOICE ID ===>', newlyCreatedInvoice._id)
@@ -67,12 +45,12 @@ console.log('invoiceToCreate===>', invoiceToCreate)
         })
         .then((updatedCustomerPurchase) => {
             console.log(updatedCustomerPurchase)
-            res.redirect('/purchases/create')
+            res.redirect('/purchases')
 
         }) 
         .catch(err => {
             console.log(err)
-            res.redirect('/purchases/create');
+            res.redirect('/purchases');
         })
     })
     
